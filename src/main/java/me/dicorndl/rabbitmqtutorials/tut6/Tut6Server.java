0664694ef82b4d0d5ba10fc8d96bc36af2de9441
dicorndl <dicorndl@gmail.com>
2019-01-25 16:00:40 +0900

    Add tutorial chapter 6

---
+++ src/main/java/me/dicorndl/rabbitmqtutorials/tut6/Tut6Server.java
@@ -0,0 +1,18 @@
+package me.dicorndl.rabbitmqtutorials.tut6;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+
+public class Tut6Server {
+
+  @RabbitListener(queues = "tut.rpc.requests")
+  public int fibonacci(int n) {
+    System.out.println(" [x] Received request for " + n);
+    int result = fib(n);
+    System.out.println(" [.] Returned " + result);
+    return result;
+  }
+
+  public int fib(int n) {
+    return n == 0 ? 0 : n == 1 ? 1 : (fib(n - 1) + fib(n - 2));
+  }
+}
