13c36ef788b98d1be27fbcde4a3672743b8fe035
dicorndl <dicorndl@gmail.com>
2019-01-30 16:43:43 +0900

    ConcurrentConsumer 테스트

--- src/main/java/me/dicorndl/rabbitmqtutorials/tut1/Tut1Config.java
+++ src/main/java/me/dicorndl/rabbitmqtutorials/tut1/Tut1Config.java
@@ -1,12 +1,17 @@
 package me.dicorndl.rabbitmqtutorials.tut1;
 
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.RabbitListenerContainerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Profile({"tut1", "hello-world"})
 @Configuration
+@EnableRabbit
 public class Tut1Config {
 
   @Bean
@@ -24,5 +29,14 @@ public class Tut1Config {
   @Bean
   public Tut1Sender sender() {
     return new Tut1Sender();
+  }
+
+  @Bean
+  public RabbitListenerContainerFactory rabbitListenerContainerFactory(ConnectionFactory connectionFactory) {
+    SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+    factory.setConnectionFactory(connectionFactory);
+    factory.setConcurrentConsumers(5);
+
+    return factory;
   }
 }
--- src/main/java/me/dicorndl/rabbitmqtutorials/tut1/Tut1Receiver.java
+++ src/main/java/me/dicorndl/rabbitmqtutorials/tut1/Tut1Receiver.java
@@ -5,13 +5,15 @@ import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 
-@RabbitListener(queues = "hello")
+@RabbitListener(queues = "hello", containerFactory = "rabbitListenerContainerFactory")
 public class Tut1Receiver {
 
   private static final Logger LOG = LoggerFactory.getLogger(Tut1Receiver.class);
 
   @RabbitHandler
-  public void receive(String in) {
+  public void receive(String in) throws InterruptedException {
+    LOG.info(" [.] Receive '" + in + "', working...");
+    Thread.sleep(3000);
     LOG.info(" [x] Received '" + in + "'");
   }
 }
